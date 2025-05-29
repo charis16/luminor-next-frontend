fae7a457696c40b8e9738e6a189ad85b1951faaa
charisredcomm <muhammad.charis@redcomm.co.id>
2025-05-29 19:27:11 +0700

    ref: fixing start.sh

--- start.sh
+++ start.sh
@@ -3,9 +3,9 @@
 # Skrip untuk build dan menjalankan Docker Compose
 
 # Hentikan dan hapus container lama (opsional, untuk menjaga kebersihan)
-docker compose down
+docker compose-down
 
 # Bangun ulang image dengan Docker Compose
 echo "Membangun ulang image dan menjalankan container..."
-docker compose up --build -d
+docker compose-up --build -d
 

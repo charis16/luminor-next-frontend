f0353c5e580a0e8db41685c52e3d9e5864355fcb
charisredcomm <muhammad.charis@redcomm.co.id>
2025-06-01 20:46:22 +0700

    fix

--- src/app/[locale]/(public)/[name]/_components/grid-album.tsx
+++ src/app/[locale]/(public)/[name]/_components/grid-album.tsx
@@ -29,25 +29,27 @@ export default function GridAlbum({ showTab }: GridAlbumProps) {
 
   return (
     <div className="mx-auto flex flex-col gap-6 py-6">
-      <div className="sticky top-[70px] z-30 transition-colors duration-300 backdrop-blur-sm py-4 bg-[#16151D]/50">
-        <div className="overflow-x-auto whitespace-nowrap scrollbar-hide">
-          <Tabs
-            aria-label="Tabs category"
-            selectedKey={selected}
-            size="md"
-            variant="underlined"
-            onSelectionChange={(key) => setSelected(key.toString())}
-          >
-            <Tab key="all" className="text-xl" title="All" />
-            <Tab key="wedding" className="text-xl" title="Wedding" />
-            <Tab key="pre-wedding" className="text-xl" title="Pre Wedding" />
-            <Tab key="family" className="text-xl" title="Family" />
-            <Tab key="maternity" className="text-xl" title="Maternity" />
-            <Tab key="engagement" className="text-xl" title="Engagement" />
-            {/* Tambah lebih banyak tab jika perlu */}
-          </Tabs>
+      {showTab && (
+        <div className="sticky top-[70px] z-30 transition-colors duration-300 backdrop-blur-sm py-4 bg-[#16151D]/50">
+          <div className="overflow-x-auto whitespace-nowrap scrollbar-hide">
+            <Tabs
+              aria-label="Tabs category"
+              selectedKey={selected}
+              size="md"
+              variant="underlined"
+              onSelectionChange={(key) => setSelected(key.toString())}
+            >
+              <Tab key="all" className="text-xl" title="All" />
+              <Tab key="wedding" className="text-xl" title="Wedding" />
+              <Tab key="pre-wedding" className="text-xl" title="Pre Wedding" />
+              <Tab key="family" className="text-xl" title="Family" />
+              <Tab key="maternity" className="text-xl" title="Maternity" />
+              <Tab key="engagement" className="text-xl" title="Engagement" />
+              {/* Tambah lebih banyak tab jika perlu */}
+            </Tabs>
+          </div>
         </div>
-      </div>
+      )}
 
       <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
         {Array.from({ length: 100 }, (_, index) => (

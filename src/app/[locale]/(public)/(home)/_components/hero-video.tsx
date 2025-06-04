00befef36de95868f68c6ad5165faa8fb8da2653
charisredcomm <muhammad.charis@redcomm.co.id>
2025-06-04 12:17:39 +0700

    fix

--- src/app/[locale]/(public)/(home)/_components/hero-video.tsx
+++ src/app/[locale]/(public)/(home)/_components/hero-video.tsx
@@ -27,6 +27,7 @@ export default function HeroVideo() {
           muted
           playsInline
           className="absolute top-0 left-0 w-full h-full object-cover"
+          preload="auto"
         >
           <source src={videoUrl} type="video/mp4" />
           Your browser does not support the video tag.

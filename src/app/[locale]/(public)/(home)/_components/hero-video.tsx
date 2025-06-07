fd5f50a0446222132eab35f71759308d10064835
charisredcomm <muhammad.charis@redcomm.co.id>
2025-06-07 13:51:44 +0700

    feat

--- src/app/[locale]/(public)/(home)/_components/hero-video.tsx
+++ src/app/[locale]/(public)/(home)/_components/hero-video.tsx
@@ -3,6 +3,7 @@
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import { VideoOff } from "lucide-react";
 import { useTranslations } from "next-intl";
+import { useRef, useEffect } from "react";
 
 import { useWebsites } from "../../_hooks/use-website";
 import EmptyState from "../../_components/empty-state";
@@ -19,20 +20,31 @@ export default function HeroVideo() {
 
   const videoUrl = isMobile ? data?.data?.video_mobile : data?.data?.video_web;
 
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    const playPromise = videoRef.current?.play();
+
+    if (playPromise !== undefined) {
+      playPromise.catch((error) => {
+        console.warn("Autoplay prevented:", error);
+      });
+    }
+  }, []);
+
   return (
     <motion.section className="relative w-full min-h-dvh" style={{ opacity }}>
       {videoUrl ? (
         <video
+          ref={videoRef}
           autoPlay
           loop
           muted
           playsInline
           className="absolute top-0 left-0 w-full h-full object-cover"
           preload="auto"
-        >
-          <source src={videoUrl} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+          src={videoUrl}
+        />
       ) : (
         <div className="absolute inset-0 flex flex-col items-center justify-center gap-2  text-white">
           <EmptyState

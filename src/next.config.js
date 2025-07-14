ec8e014798670620366eb6faaf9d0d1cf13a6bb5
charisredcomm <muhammad.charis@redcomm.co.id>
2025-07-14 16:28:28 +0700

    feat

--- src/next.config.js
+++ src/next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
   images: {
+    domains: ["cdn.luminorpictures.com"],
     remotePatterns: [
       {
         protocol: "https",
